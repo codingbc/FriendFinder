3ced40eb7c21beb7d57fd6ebe35dda09a82ef902
Nida <codingbc@gmail.com>
2018-07-14 14:11:55 -0400

    commit init

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "http://solwee.org/static/media/default-avatar.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Jacob",
+        "photo": "http://solwee.org/static/media/default-avatar.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremiah",
+        "photo": "http://solwee.org/static/media/default-avatar.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Louis",
+        "photo": "http://solwee.org/static/media/default-avatar.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Lou",
+        "photo": "http://solwee.org/static/media/default-avatar.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Jordan",
+        "photo": "http://solwee.org/static/media/default-avatar.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+
+]
+
+module.exports = friends;
